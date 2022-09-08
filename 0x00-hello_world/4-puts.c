253a9e2d004039867f48daff8b5a9de52675a261
Mukhtar Ibrahim <pr.shiin1@gmail.com>
2022-09-08 12:37:03 -0700

    add: 4-puts.c file, prints text using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ ** main - prints text using puts funtion
+ ** just adhering to betty-style
+ ** Return: 0 if successful else 1
+  */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
