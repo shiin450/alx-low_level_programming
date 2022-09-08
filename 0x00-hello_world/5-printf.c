414f26bcb6a972f7b2407778bac4b3a54d4484b5
Mukhtar Ibrahim <pr.shiin1@gmail.com>
2022-09-08 12:49:25 -0700

    add: 5-printf, prints text using prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ ** main - prints text using putf funtion
+ ** just adhering to betty-style
+ ** Return: 0 if successful else 1
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
