553602726cf196d032a9d1ae10734ef99d42b4ed
Mukhtar Ibrahim <pr.shiin1@gmail.com>
2022-09-08 21:15:07 +0000

    prints to the stderr using fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ ** main - prints text to stderr using fprintf
+ ** just adhering to betty-style
+ ** Return: 1 if successful else 0
+  */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
