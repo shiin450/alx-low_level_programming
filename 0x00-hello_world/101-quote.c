ba141bee91b10ad28c9fc9b05db3ca6a61c1c56d
Mukhtar Ibrahim <pr.shiin1@gmail.com>
2022-09-08 22:17:37 +0000

    fix: 101-quote.c, use system call instead of fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  ** main - prints text to stderr using fprintf
  ** just adhering to betty-style
@@ -6,6 +6,7 @@
   */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+									59);
 	return (1);
 }
